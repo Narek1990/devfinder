ecd29c15095c7909047388e12a87a282a5c783e4
Narek <hovsepyan090990@gmail.com>
2022-07-18 16:27:29 +0400

    from workplace2

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -6,32 +6,42 @@ import WebIcon from "../../resources/icons/IconWebsite";
 import CompanyIcon from "../../resources/icons/IconCompany";
 
 
-export const Main = () => {
-  return <div className="main-container">
-<img src="" alt="" className="logo"/>
-<div className="main-data">
-  
-  <div className="data-header">
-    <p className="title">The Octocat</p>
-  <p className="data-time">Joined 25 Jan 2011</p>
 
-</div>
-<p className="title1">@octocat</p>
-<p className="title2">This profile has no bio</p>
-<div className="data-body">
-<span>Repos</span>
-<span>Followers</span>
-<span>Following</span>
-<span className="calculated-data">8</span>
-<span className="calculated-data">3938</span>
-<span className="calculated-data"></span>
-  </div>
-  <div className="data-footer">
- <span className="footer"><LocationIcon className="icons"/> Francisco</span>
- <span className="footer"><TwitterIcon className="icons"/>Not available</span>
- <span className="footer"><WebIcon className="icons"/>https://github.blog</span>
- <span className="footer"><CompanyIcon className="icons"/>@github</span>
-  </div>
-  </div>
+
+const dateFormatter = (date) => {
+  return new Date(date).toDateString()
+}
+
+
+export const Main = ({ data }) => {
+  return <div className="main-container">
+    {data ? (
+      <React.Fragment>
+        <img src={data.avatar_url} alt="" className="logo" />
+        <div className="main-data">
+          <div className="data-header">
+            <p className="title">{data.name}</p>
+            <p className="data-time">Joined {dateFormatter(data.created_at)}</p>
+
+          </div>
+          <p className="title1">@octocat</p>
+          <p className="title2">This profile has no bio</p>
+          <div className="data-body">
+            <span>Repos</span>
+            <span>Followers</span>
+            <span>Following</span>
+            <span className="calculated-data">{data.public_repos}</span>
+            <span className="calculated-data">{data.followers}</span>
+            <span className="calculated-data">{data.following}</span>
+          </div>
+          <div className="data-footer">
+            <span className="footer"><LocationIcon className="icons" />{data.location}</span>
+            <span className="footer"><TwitterIcon className="icons" />{data.twitter_username}</span>
+            <span className="footer"><WebIcon className="icons" />{data.blog}</span>
+            <span className="footer"><CompanyIcon className="icons" />{data.company}</span>
+          </div>
+        </div>
+      </React.Fragment>
+    ) : null}
   </div>
 };
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -5,25 +5,25 @@ import { Header } from "../Components/Header/Header";
 import { Main } from "../Components/Main/Main";
 import { Searchbar } from "../Components/SearchBar/Searchbar";
 
-const ApiUrl = "https://api.github.com/users/";
+const ApiUrl = "https://api.github.com/users/octocat";
 
 const Layout = () => {
-  const [userName, setUserName] = useState("octocat");
+  const [userData, setUserData] = useState(null);
 
   const getUserInfo = async () => {
-    const res = await axios.get(`${ApiUrl}${userName}`);
-    const { data } = res;
+    const res = await axios.get(`${ApiUrl}`);
+    setUserData(res.data)
   };
 
   useEffect(() => {
     getUserInfo();
-  }, [userName]);
+  }, [])
 
   return (
     <div className="Layout">
       <Header />
       <Searchbar />
-      <Main />
+      <Main data={userData} />
     </div>
   );
 };
